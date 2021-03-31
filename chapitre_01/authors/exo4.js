76e1568908f9736acf57320caeb02e346949a4a3
Massou <massou.lionel@gmail.com>
2021-03-31 12:29:30 +0200

    backend jour-1 TMP

--- chapitre_01/authors/exo4.js
+++ chapitre_01/authors/exo4.js
@@ -4,71 +4,71 @@ var port = 8000;
 
 // EXERCICE 4
 
-app.get('/authors/:id', (req, res) => {
-    var authors = req.params.number;
-    var books = "";
+// app.get('/authors/:id', (req, res) => {
+//     var authors = req.params.number;
+//     var books = "";
     
-    switch (authors) {
+//     switch (authors) {
 
-        case "1":
-            books = "Beowulf";
-            break;
+//         case "1":
+//             books = "Beowulf";
+//             break;
 
-        case "2":
-            books = "Hamlet, Othello, Romeo and Juliet, MacBeth";
-            break;
+//         case "2":
+//             books = "Hamlet, Othello, Romeo and Juliet, MacBeth";
+//             break;
 
-        case "3":
-            books = "Oliver Twist, A Christmas Carol";
-            break;
+//         case "3":
+//             books = "Oliver Twist, A Christmas Carol";
+//             break;
 
-        case "4":
-            books = "The Picture of Dorian Gray, The Importance of Being Earnest";
-            break;
+//         case "4":
+//             books = "The Picture of Dorian Gray, The Importance of Being Earnest";
+//             break;
 
-        default:
-            books = "not found"
-            break;
-    }
+//         default:
+//             books = "not found"
+//             break;
+//     }
 
-    res.json({
-        books: books,
-    });
-});
+//     res.json({
+//         books: books,
+//     });
+// });
 
-app.get('/authors/:id/books/', (req, res) => {
-    var authors = req.params.number;
-    var books = "";
+// app.get('/authors/:id/books/', (req, res) => {
+//     var authors = req.params.number;
+//     var books = "";
     
-    switch (authors) {
+//     switch (authors) {
 
-        case "1":
-            books = "Beowulf";
-            break;
+//         case "1":
+//             books = "Beowulf";
+//             break;
 
-        case "2":
-            books = "Hamlet, Othello, Romeo and Juliet, MacBeth";
-            break;
+//         case "2":
+//             books = "Hamlet, Othello, Romeo and Juliet, MacBeth";
+//             break;
 
-        case "3":
-            books = "Oliver Twist, A Christmas Carol";
-            break;
+//         case "3":
+//             books = "Oliver Twist, A Christmas Carol";
+//             break;
 
-        case "4":
-            books = "The Picture of Dorian Gray, The Importance of Being Earnest";
-            break;
+//         case "4":
+//             books = "The Picture of Dorian Gray, The Importance of Being Earnest";
+//             break;
 
-        default:
-            books = "not found"
-            break;
-    }
+//         default:
+//             books = "not found"
+//             break;
+//     }
 
-    res.json({
-        books: books,
-    });
-});
+//     res.json({
+//         books: books,
+//     });
+// });
 
-app.listen(port, function () {
-    console.log('Serveur lancé et en écoute dans le port: ' + port);
-});
+// app.listen(port, function () {
+//     console.log('Serveur lancé et en écoute dans le port: ' + port);
+// });
 
