d21dafec5fc3cf4075a2afcf9995054a6a07ac1b
Lionel <massou.lionel@gmail.com>
2021-04-30 16:08:40 +0200

    country finish

--- api-countries-react/server.js
+++ api-countries-react/server.js
@@ -8,14 +8,34 @@ const port = 8000;
 
 app.get("/countries", function (req, res) {
 
-    console.log("countries", countries)
+    // console.log("countries1", countries)
 
-    res.json({
+    res.json({ 
         countries
-    })
-
-  
+    })  
+    // console.log("countries2", countries);
 })
+
+app.get("/countries/:name",function (req,res) {
+
+    let oneCountry = req.params.name.toUpperCase()
+    
+    console.log(oneCountry);
+
+    const arrayCountry=[]
+
+    for (let i=0;i<countries.length;i++){
+
+        const currentCountry = countries[i]
+
+        if(oneCountry===currentCountry.name.toUpperCase()){
+          arrayCountry.push(currentCountry)
+    }
+   }
+     res.json({arrayCountry})
+
+})
+
 
 app.listen(port, function () {
     console.log('Serveur lancé et en écoute dans le port: ' + port);
