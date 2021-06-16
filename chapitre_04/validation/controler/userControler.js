38229e14fd046ba25cce3f16b647e77c1e8a0fce
lionelmassou <massou.lionel@gmail.com>
2021-06-16 16:56:03 +0200

    chap04_Validation Attempt04

--- chapitre_04/validation/controler/userControler.js
+++ chapitre_04/validation/controler/userControler.js
@@ -1,4 +1,52 @@
 const userModel = require('../model/userModel')
+const expressValidator = require("express-validator");
 
 
-module.exports = {}
+const userValidationAndAdd = async (req, res) => {
+    try {
+        expressValidator.body("username").exists().isLength({ min: 4 }),
+            expressValidator.body("mail").exists().isEmail(),
+            expressValidator.body("age").exists().isNumeric({ min: 0, max: 99 }).isLength({ max: 2 }),
+            expressValidator.body("city").isIn(['Paris', 'Tokyo', 'Los Angeles']),
+            // expressValidator.body("password").custom((value) => {
+            //     var schema = new passwordValidator();
+            //     schema
+            //         .is().min(8) // Minimum length 8
+            //         .is().max(100) // Maximum length 100
+            //         .has().uppercase() // Must have uppercase letters
+            //         .has().lowercase() // Must have lowercase letters
+            //         .has().digits(2) // Must have at least 2 digits
+            //         .has().not().spaces() // Should not have spaces
+            //         .is().not().oneOf(["Passw0rd", "Password123"]);
+            //     return schema.validate(value);
+            // }),
+            (req, res) => {
+                const errors = expressValidator.validationResult(req);
+                if (errors.isEmpty() === false) {
+                    res.status(400).json({
+                        errors: errors.array(), // to be used in a json loop
+                        message: 'there is a problem'
+                    });
+                    return;
+                } else {
+
+                    const user = req.body
+                    console.log("what is user: ", user)
+
+                    const newUser = User.create(user)
+
+                    res.json({
+                        success: true,
+                        message: 'User will be saved'
+
+                    });
+                }
+            }
+    } catch (err) {
+        console.log(err)
+
+        res.status(500).json({ errorMessage: "There was a problem :(" })
+    }
+}
+
+module.exports = { userValidationAndAdd }
--- chapitre_04/validation/userRoute.js
+++ chapitre_04/validation/userRoute.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const UserModel = require("./model/userModel")
 const debug = require("./middleware/debug")
 
-const expressValidator = require("express-validator");
+const { userValidationAndAdd } = require('./controler/userControler')
+
+// const expressValidator = require("express-validator");
 // const passwordValidator = require('password-validator');
 
 const app = express();
@@ -15,40 +17,11 @@ router.get("/", (req, res) => {
     res.json({ message: "all routes" })
 })
 
-router.post('/users/add', debug,
-    expressValidator.body("username").exists().isLength({ min: 4 }),
-    expressValidator.body("mail").exists().isEmail(),
-    expressValidator.body("age").exists().isNumeric({ min: 0, max: 99 }).isLength({ max: 2 }),
-    expressValidator.body("city").isIn(['Paris', 'Tokyo', 'Los Angeles']),
-    // expressValidator.body("password").custom((value) => {
-    //     var schema = new passwordValidator();
-    //     schema
-    //         .is().min(8) // Minimum length 8
-    //         .is().max(100) // Maximum length 100
-    //         .has().uppercase() // Must have uppercase letters
-    //         .has().lowercase() // Must have lowercase letters
-    //         .has().digits(2) // Must have at least 2 digits
-    //         .has().not().spaces() // Should not have spaces
-    //         .is().not().oneOf(["Passw0rd", "Password123"]);
-    //     return schema.validate(value);
-    // }),
-    (req, res) => {
-        const errors = expressValidator.validationResult(req);
-        if (errors.isEmpty() === false) {
-            res.status(400).json({
-                errors: errors.array(), // to be used in a json loop
-                message: 'there is a problem'
-            });
-            return;
-        } else {
+router.post('/users/add', debug, userValidationAndAdd);
 
-            res.json({
-                success: true,
-                message: 'User will be saved'
-            });
-        }
-    }
-);
+router.get('/users/:username',debug)
+
+router.get('/users/:email',debug)
 
 router.all("*", (req, res) => {
     res.json({
